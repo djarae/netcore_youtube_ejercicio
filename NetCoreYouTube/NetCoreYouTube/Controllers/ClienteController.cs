413c2a963ebb264ecc3184d33d18b51bd22d4b74
djarae <djaraestivill@gmail.com>
2024-09-01 21:33:57 -0400

    RESPALDO

---
+++ NetCoreYouTube/NetCoreYouTube/Controllers/ClienteController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NetCoreYouTube.Models;
+using System.Numerics;
+
+namespace NetCoreYouTube.Controllers
+
+
+{
+
+    [ApiController]
+    [Route("cliente")]
+    public class CllienteController : ControllerBase
+    {
+
+        [HttpGet]
+        [Route("listar")]
+        public dynamic listarCliente(int idCliente )
+        {
+            //Todo el codigo que quieras
+            List<Cliente> clientes = new List<Cliente>
+            {
+                new Cliente
+                {
+                    id = "1",
+                    nombre = "Diego Jara",
+                    edad = "30",
+                    correo = "correoprueba@gmail.com"
+
+                },
+                new Cliente
+                {
+                    id = "2",
+                    nombre="Pedro diaz",
+                    edad ="20",
+                    correo ="a@gmail.com"
+                }
+
+            };
+            //return clientes;
+            return idCliente;
+
+        }
+
+        [HttpPost]
+        [Route("guardar")]
+        public dynamic guardarCliente(Cliente cliente)
+        {
+            //cliente.id = "3";
+            return new
+            {
+
+                success = true,
+                message = "Cliente Registrado",
+                result = cliente.id
+            };
+        }
+    }
+}
